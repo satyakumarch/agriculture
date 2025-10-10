8dde673ae93c7c92f9720f5d637ea7e92092bdfc
SatyaKumar <satyakumarchaudhary603@gmail.com>
2025-10-10 20:48:38 +0530

    fix the postcss config setting

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,7 @@ export default {
     autoprefixer: {},
   },
 }
+// we are creating a react app with vite and typescript and tailwindcss
+// we are using react-router-dom for routing
+// we are using framer-motion for animations
+// we are using context api for state management
