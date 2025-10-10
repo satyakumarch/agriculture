137c2c4ce5a243cd6d33ae902c0bb8c45f0a6621
SatyaKumar <satyakumarchaudhary603@gmail.com>
2025-10-10 18:46:21 +0530

    updating the website on the monitoring the sensor

--- src/App.tsx
+++ src/App.tsx
@@ -35,3 +35,8 @@ const App = () => {
 };
 
 export default App;
+
+// we are creating a react app with vite and typescript and tailwindcss
+// we are using react-router-dom for routing
+// we are using framer-motion for animations
+// we are using context api for state management
